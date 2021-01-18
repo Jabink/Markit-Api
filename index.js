b61412987f02b33e61b0aaebae94adc9b2a1da4f
Jabin <jabinfreelance@gmail.com>
2021-01-18 23:19:38 +0530

    api route added

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-// require("dotenv").config();
+require("dotenv").config();
 const express = require("express");
-// const fileUpload = require("express-fileupload");
+const fileUpload = require("express-fileupload");
 const app = express();
 
-// require("dotenv").config();
-// require("./db/connection").connect();
+require("dotenv").config();
+require("./db/connection").connect();
 
 
 //middleware
-// app.use(express.json());
-// app.use(fileUpload());
-// app.use("/api/category",require("./routes/Category"));
-// app.use("/api/product",require("./routes/Product"));
-// app.use("/api/shop/ads", require("./routes/AdsRoute"));
-// app.use("/api/auth", require("./routes/AuthRoute"));
-// app.use("/upload", express.static("./uploads"));
+app.use(express.json());
+app.use(fileUpload());
+app.use("/api/category",require("./routes/Category"));
+app.use("/api/product",require("./routes/Product"));
+app.use("/api/shop/ads", require("./routes/AdsRoute"));
+app.use("/api/auth", require("./routes/AuthRoute"));
+app.use("/upload", express.static("./uploads"));
 app.get("/",(req,res)=> res.send("Page opened"));
 
 app.listen(process.env.PORT || 3000, () => console.log("Server running"))
